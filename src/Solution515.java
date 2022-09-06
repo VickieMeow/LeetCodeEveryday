2fe03d5dfdf9116e2c28df2984ae8560b17fa2a0
miaomeiqi <miaomiaomiaoyeah@163.com>
2022-09-06 17:54:04 +0800

    feat solution515

--- src/Solution515.java
+++ src/Solution515.java
@@ -1,2 +1,40 @@
-package PACKAGE_NAME;public class Solution515 {
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.List;
+
+/**
+ * 在每个树行中找最大值
+ * @author miaomeiqi
+ */
+public class Solution515 {
+    class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+    }
+    public List<Integer> largestValues(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        Deque<TreeNode> deque = new ArrayDeque<>();
+        if (root == null) {
+            return res;
+        }
+        deque.offer(root);
+        while (!deque.isEmpty()) {
+            int size = deque.size();
+            int curMaxNum = Integer.MIN_VALUE;
+            for (int i = 0; i < size; i++) {
+                TreeNode curNode = deque.pollFirst();
+                if (curNode.left != null) {
+                    deque.offer(curNode.left);
+                }
+                if (curNode.right != null) {
+                    deque.offer(curNode.right);
+                }
+                curMaxNum = Math.max(curNode.val, curMaxNum);
+            }
+            res.add(curMaxNum);
+        }
+        return res;
+    }
 }
